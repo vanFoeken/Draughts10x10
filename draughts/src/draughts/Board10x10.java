02b59f753ebc6cb792f00abc6ea2cb27654f4192
vanFoeken <106931123+vanFoeken@users.noreply.github.com>
2025-11-06 06:25:09 +0100

    Refactor Board10x10 class and update colors

--- draughts/src/draughts/Board10x10.java
+++ draughts/src/draughts/Board10x10.java
@@ -31,7 +31,7 @@ final class Board10x10 extends JPanel implements ActionListener {
     final static int GRID = 10;//10x10
 
     final private static Color LIGHT = new Color(255, 250, 188);//board
-    final private static Color DARK = new Color(185, 135, 95);//tile
+    final private static Color DARK = new Color(186, 136, 96);//tile
     
     final Rectangle[] tile = new Rectangle[GRID * GRID / 2];//[50]
     
@@ -91,6 +91,7 @@ final class Board10x10 extends JPanel implements ActionListener {
     }
 
 }
+
 
 
 
