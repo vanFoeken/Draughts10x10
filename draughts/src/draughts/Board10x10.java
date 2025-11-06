e10b48b0812f385085383cf1847e14f482a40b45
vanFoeken <106931123+vanFoeken@users.noreply.github.com>
2025-11-06 05:51:12 +0100

    Update Board10x10.java

--- draughts/src/draughts/Board10x10.java
+++ draughts/src/draughts/Board10x10.java
@@ -30,7 +30,7 @@ import javax.swing.JPanel;
 final class Board10x10 extends JPanel implements ActionListener {    
     final static int GRID = 10;//10x10
 
-    final private static Color LIGHT = new Color(255, 250, 200);//board
+    final private static Color LIGHT = new Color(255, 250, 195);//board
     final private static Color DARK = new Color(184, 134, 94);//tile
     
     final Rectangle[] tile = new Rectangle[GRID * GRID / 2];//[50]
@@ -91,5 +91,6 @@ final class Board10x10 extends JPanel implements ActionListener {
     }
 
 }
+
 
 
