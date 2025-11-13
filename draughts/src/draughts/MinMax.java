697306f1c6c925d025948e5ba18d7155ea6b72c7
vanFoeken <106931123+vanFoeken@users.noreply.github.com>
2025-11-13 22:13:01 +0100

    Update MinMax.java

--- draughts/src/draughts/MinMax.java
+++ draughts/src/draughts/MinMax.java
@@ -32,7 +32,7 @@ import java.util.HashSet;
  */
 
 final class MinMax extends HashMap<String, Integer> {
-    final private static int ALFA =  Integer.MAX_VALUE;
+    final private static int ALFA = Integer.MAX_VALUE;
     final private static int BETA = Integer.MIN_VALUE;
     
     private static enum Node {
@@ -381,5 +381,6 @@ final class MinMax extends HashMap<String, Integer> {
     }
     
 }
+
 
 
